e97fefc6d78518e606a3b8a75b755a992a8da688
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-06-09 20:36:26 -0300

    feat: conan 2.0 support for opentracing-cpp

--- opentracing-cpp/conanfile.py
+++ opentracing-cpp/conanfile.py
@@ -1,39 +1,62 @@
-from conans import ConanFile, CMake, tools
+import os
 
+from conan import ConanFile
+from conan.tools.files import copy, get, rmdir
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
-class OpentracingcppConan(ConanFile):
+class OpenTracingCppConan(ConanFile):
     name = "opentracing-cpp"
     version = "1.4.0"
     license = ""
     url = "https://github.com/labviros/is-packages"
     description = ""
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=True", "fPIC=True"
-    generators = "cmake"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared" : True,
+        "fPIC": True,
+    }
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        self.run("git clone https://github.com/opentracing/opentracing-cpp")
-        self.run("cd opentracing-cpp && git checkout v1.4.0")
-        tools.replace_in_file(
-            "opentracing-cpp/CMakeLists.txt", "project(opentracing-cpp)",
-            '''project(opentracing-cpp)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        cmake = CMakeToolchain(self)
+        cmake.variables["BUILD_TESTING"] = "OFF"
+        cmake.variables["ENABLE_LINTING"] = "OFF"
+        cmake.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.variables["BUILD_STATIC_LIBS"] = not self.options.shared
+        if not self.options.shared:
+            cmake.variables["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        cmake.definitions["BUILD_STATIC_LIBS"] = "OFF" if self.options.shared else "ON"
-        if not self.options.shared:
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-
-        cmake.configure(source_folder="opentracing-cpp")
+        cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        pass
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+
 
     def package_info(self):
-        self.cpp_info.libs = ["opentracing"]
+        self.cpp_info.set_property("cmake_file_name", "opentracing")
+        self.cpp_info.components["opentracing"].set_property("cmake_file_name", "opentracing")
+        self.cpp_info.components["opentracing"].set_property("cmake_target_name", "opentracing::opentracing")
+        self.cpp_info.components["opentracing"].set_property("pkg_config_name", "opentracing")
+        self.cpp_info.components["opentracing"].libs = ["opentracing"]
+
+
