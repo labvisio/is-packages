5f6069e2d8a49487af849820b3136515786f8fd4
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-06-12 10:40:21 -0300

    feat: conan 2.0 support for zipkin-cpp-opentracing

--- zipkin-cpp-opentracing/build.py
+++ zipkin-cpp-opentracing/build.py
@@ -1,9 +1,28 @@
-from conan.packager import ConanMultiPackager
+import subprocess
+
+
+def main():
+    command = """
+        conan create . --user is --channel stable \
+            -s compiler=gcc \
+            -s compiler.version=11 \
+            -s compiler.libcxx=libstdc++11 \
+            -s build_type=Release \
+            -b missing \
+            -o:h zipkin-cpp-opentracing/*:shared=True \
+    """
+    subprocess.call(['bash', '-c', command])
+    command = """
+        conan create . --user is --channel stable \
+            -s compiler=gcc \
+            -s compiler.version=11 \
+            -s compiler.libcxx=libstdc++11 \
+            -s build_type=Debug \
+            -b missing \
+            -o:h zipkin-cpp-opentracing/*:shared=True \
+    """
+    subprocess.call(['bash', '-c', command])
+
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_policy="missing")
-
-    builder.add({"compiler.libcxx": "libstdc++11", "build_type": "Release"})
-    builder.add({"compiler.libcxx": "libstdc++11", "build_type": "Debug"})
-
-    builder.run()
+    main()
--- zipkin-cpp-opentracing/conanfile.py
+++ zipkin-cpp-opentracing/conanfile.py
@@ -1,42 +1,72 @@
-from conans import ConanFile, CMake, tools
+import os
+
+from conan import ConanFile
+from conan.tools.files import copy, get, rmdir
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 
 
 class ZipkincppopentracingConan(ConanFile):
     name = "zipkin-cpp-opentracing"
     version = "0.3.1"
-    license = ""
-    url = "https://github.com/labviros/is-packages"
-    description = ""
+    license = "Apache-2.0"
+    url = "https://github.com/labvisio/is-packages"
+    homepage = "https://github.com/rnburn/zipkin-cpp-opentracing"
+    description = "OpenTracing Tracer implementation for Zipkin in C++"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=True", "fPIC=True"
-    generators = "cmake"
-    requires = ("libcurl/7.61.1@bincrafters/stable", "opentracing-cpp/1.4.0@is/stable")
+    options = {
+        "shared": [True],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": True,
+        "fPIC": True,
+    }
 
     def configure(self):
-        self.options["libcurl"].shared = self.options.shared 
-        self.options["opentracing-cpp"].shared = self.options.shared 
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.options["libcurl"].shared = self.options.shared
+        self.options["opentracing-cpp"].shared = self.options.shared
+
+    def requirements(self):
+        self.requires("libcurl/7.76.0")
+        self.requires("opentracing-cpp/1.4.0@is/stable")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        self.run("git clone https://github.com/rnburn/zipkin-cpp-opentracing")
-        self.run("cd zipkin-cpp-opentracing && git checkout v0.3.1")
-        tools.replace_in_file(
-            "zipkin-cpp-opentracing/CMakeLists.txt", "project(zipkin-opentracing)",
-            '''project(zipkin-opentracing)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.variables["BUILD_STATIC_LIBS"] = not self.options.shared
+        tc.variables["BUILD_TESTING"] = False
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTING"] = "OFF"
-        if not self.options.shared:
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
-        cmake.configure(source_folder="zipkin-cpp-opentracing")
+        cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        pass
+        copy(self, "LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        cmake = CMake(self)
+        cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
 
     def package_info(self):
-        self.cpp_info.libs = ["zipkin_opentracing", "zipkin"]
+        target = "zipkin-cpp-opentracing"
+        self.cpp_info.set_property("pkg_config_name", target)
+        self.cpp_info.set_property("cmake_file_name", target)
+        self.cpp_info.set_property("cmake_target_name", f"zipkin-cpp-opentracing::{target}")
+        self.cpp_info.components["zipkin-cpp-opentracing"].libs = ["zipkin", "zipkin_opentracing"]
+        self.cpp_info.components["zipkin-cpp-opentracing"].requires = [
+            "opentracing-cpp::opentracing-cpp",
+            "libcurl::libcurl",
+        ]
