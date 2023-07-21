8f8627013597aad1ae9e11f99a9c566dbf6a6dcc
Luiz Carlos Cosmi Filho <luizcarloscosmifilho@gmail.com>
2023-07-21 17:20:20 -0300

    chore: update build script opentracing-cpp

--- recipes/opentracing-cpp/build.py
+++ recipes/opentracing-cpp/build.py
@@ -2,46 +2,20 @@ import subprocess
 
 
 def main():
-    command = """
-        conan create . --user is --channel stable \
-            -s compiler=gcc \
-            -s compiler.version=11 \
-            -s compiler.libcxx=libstdc++11 \
-            -s build_type=Release \
-            -b missing \
-            -o:h opentracing-cpp/*:shared=False
-    """
-    subprocess.call(['bash', '-c', command])
-    command = """
-        conan create . --user is --channel stable \
-            -s compiler=gcc \
-            -s compiler.version=11 \
-            -s compiler.libcxx=libstdc++11 \
-            -s build_type=Release \
-            -b missing \
-            -o:h opentracing-cpp/*:shared=True
-    """
-    command = """
-        conan create . --user is --channel stable \
-            -s compiler=gcc \
-            -s compiler.version=11 \
-            -s compiler.libcxx=libstdc++11 \
-            -s build_type=Debug \
-            -b missing \
-            -o:h opentracing-cpp/*:shared=False
-    """
-    subprocess.call(['bash', '-c', command])
-    command = """
-        conan create . --user is --channel stable \
-            -s compiler=gcc \
-            -s compiler.version=11 \
-            -s compiler.libcxx=libstdc++11 \
-            -s build_type=Debug \
-            -b missing \
-            -o:h opentracing-cpp/*:shared=True
-    """
-    subprocess.call(['bash', '-c', command])
-
+    options_shared = [True, False]
+    options_build_type = ["Debug", "Release"]
+    for shared in options_shared:
+        for build_type in options_build_type:
+            command = f"""
+                conan create . --user is --channel stable \
+                    -s compiler=gcc \
+                    -s compiler.version=11 \
+                    -s compiler.libcxx=libstdc++11 \
+                    -s build_type={build_type} \
+                    -b missing \
+                    -o:h opentracing-cpp/*:shared={shared}
+            """
+            subprocess.call(['bash', '-c', command])
 
 if __name__ == "__main__":
     main()
